cc0b6abebafacaad1ae7f0a5efda173ed9727a92
ericcrick <nanaoseieric6@gmail.com>
2022-06-30 09:29:39 -0400

    added package references to csproj

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,9 @@ namespace JwtAuthentication.Models
 {
     public class User
     {
-        public string Username { set; get; } = string.Empty;
-        public byte[] Password { set; get; } = null!;
+        public int Id { get; set;}
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
